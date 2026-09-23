a4dcb223cd27cb72514468dbf6a472441f9b1705
vb56nw5jy9-ctrl <vb56nw5jy9@privaterelay.appleid.com>
2026-09-23 14:00:14 +0300

    Update coffee_leads.py

--- coffee_leads.py
+++ coffee_leads.py
@@ -1,20 +1,39 @@
 import feedparser
 import csv
 
-rss = "https://news.google.com/rss/search?q=new+cafe+finland"
+feeds = {
+    "New Cafes":
+        "https://news.google.com/rss/search?q=new+cafe+finland",
 
-feed = feedparser.parse(rss)
+    "New Restaurants":
+        "https://news.google.com/rss/search?q=new+restaurant+finland",
+
+    "Hotel Renovations":
+        "https://news.google.com/rss/search?q=hotel+renovation+finland",
+
+    "Barista Jobs":
+        "https://news.google.com/rss/search?q=barista+finland"
+}
 
 with open("leads.csv", "w", newline="", encoding="utf-8") as f:
     writer = csv.writer(f)
 
-    writer.writerow(["Title", "Link"])
+    writer.writerow([
+        "Category",
+        "Title",
+        "Link"
+    ])
 
-    for entry in feed.entries:
-        writer.writerow([
-            entry.title,
-            entry.link
-        ])
+    for category, rss_url in feeds.items():
 
-print("CSV file created.")
-print("CSV file created successfully")
+        feed = feedparser.parse(rss_url)
+
+        for entry in feed.entries:
+            writer.writerow([
+                category,
+                entry.title,
+                entry.link
+            ])
+
+print("Coffee leads created.")
+
